d4e77816bb76d8f32b3c4c8697ac7ad0b6306eb1
Frank Chen <Frank.Chen@amlogic.com>
2010-12-13 14:33:22 +0800

    replace the control macro

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH:=$(call my-dir)
 
-ifeq ($(strip $(TARGET_USE_NAND_FTL)),true)
+ifeq ($(strip $(TARGET_HAVE_FTL_MEDIA_PARTITION)),true)
     PRODUCT_COPY_FILES += $(LOCAL_PATH)/avnftl.ko:system/lib/avnftl.ko
 endif
