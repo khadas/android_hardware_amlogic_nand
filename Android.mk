372409cba6ce9b33db229c58df72c521260f5b63
eric <eric.zhong@amlogic.com>
2010-12-16 11:30:07 +0800

    auto copy for nand ftl lib

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH:=$(call my-dir)
 
 ifeq ($(strip $(TARGET_HAVE_FTL_MEDIA_PARTITION)),true)
-    PRODUCT_COPY_FILES += $(LOCAL_PATH)/avnftl.ko:system/lib/avnftl.ko
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/g_ftl.ko:system/lib/g_ftl.ko
 endif
